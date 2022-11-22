9662f8b23941548c025748d36bff1bf84f3e133b
bencmbrook <ben@transcend.io>
2022-11-22 13:59:40 -0600

    Try with native import.meta.resolve

--- test.mjs
+++ test.mjs
@@ -1,7 +1,6 @@
 import { resolve } from "import-meta-resolve";
 import * as remark from "remark";
 import { createRequire } from "module";
-import * as request from "request";
 
 const require = createRequire(import.meta.url);
 
@@ -15,28 +14,16 @@ console.log("- [PASS] remark:", typeof remark);
 // Successful log
 console.log("- [PASS] require.resolve:", require.resolve("remark"));
 
+// Successful log
+console.log(
+  "- [PASS] native import.meta.resolve",
+  await import.meta.resolve("remark")
+);
+
 // Broken, won't log
 try {
   const x = await resolve("remark", import.meta.url);
-  console.log("- [PASS] import-meta-resolve:", x);
+  console.log("- [PASS] import-meta-resolve ponyfill:", x);
 } catch (e) {
-  console.log("- [FAIL] import-meta-resolve:", e);
-}
-
-console.log("\n\n** CJS **");
-/**
- * CJS
- */
-// Successful log
-console.log("- [PASS] request:", typeof request);
-
-// Successful log
-console.log("- [PASS] require.resolve:", require.resolve("request"));
-
-// Broken, won't log
-try {
-  const x = await resolve("request", import.meta.url);
-  console.log("- [PASS] import-meta-resolve:", x);
-} catch (e) {
-  console.log("- [FAIL] import-meta-resolve:", e);
+  console.log("- [FAIL] import-meta-resolve ponyfill:", e);
 }
